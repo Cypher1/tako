3dc25457f2f03469bfc5fd9c89df9a80bc664eb9
Joshua Pratt <jp10010101010000@gmail.com>
2018-12-21 18:15:04 +1100

    Split interpreter out of Main and implement a Rule type (#31)

--- lib/Resolution.hs
+++ lib/Resolution.hs
@@ -4,7 +4,7 @@ import qualified Data.Set as S
 import Data.Set (Set)
 import qualified Data.Map as M
 
-import Pred (Pred (Pred), Val, Var, Atom(Value, Variable, Predicate), Assignment, getVarsFrom)
+import Pred (Pred (Pred), Val, Var, Atom(Value, Variable, Predicate, Rule), Assignment, getVarsFrom)
 
 
 type State = Set (Pred Val)
@@ -38,6 +38,17 @@ data ResolutionFailure
   | PredicatesOfDifferentShapes
     { requirement :: Pred Var
     , possible_solution :: Pred Val
+    , in_ :: Assignment Val
+    }
+  | ValueRuleMismatch
+    { val_rule :: Atom Val
+    , var_other :: Atom Var
+    , in_ :: Assignment Val
+    }
+  | VariableRuleMismatch
+    { var_rule :: Atom Var
+    , val_other :: Atom Val
+    , in_ :: Assignment Val
     }
     deriving (Eq, Ord, Show)
 
@@ -93,7 +104,6 @@ emptyState = S.empty
 solutions :: State -> Requirements -> [Assignment Val]
 solutions known preds = ignoreErrors $ solutionsAndErrors known preds
 
--- TODO(jopra): Should check that each value is defined (not just used)
 solutionsAndErrors :: State -> Requirements -> [Resolution]
 solutionsAndErrors known preds
    = map (\res -> S.foldr requireDefined res vars) $ resolution known (S.toList preds) mempty
@@ -131,11 +141,13 @@ restrictAtoms v@(Value _) (Predicate p) ass
   = (\ass' -> Error $ VariableVsPredicateMismatch {predicate = p, variable = v, in_ = ass'}) =<< ass
 restrictAtoms (Predicate p) v@(Value _) ass
   = (\ass' -> Error $ ValueVsPredicateMismatch {predicate_match = p, value = v, in_ = ass'}) =<< ass
+restrictAtoms (Rule _h1 _t1) (Rule _h2 _t2) _ass = undefined
+restrictAtoms r@(Rule _ _) o ass = (\ass' -> Error $ VariableRuleMismatch { var_rule = r, val_other = o, in_ = ass'}) =<< ass
+restrictAtoms o r@(Rule _ _) ass = (\ass' -> Error $ ValueRuleMismatch { val_rule = r, var_other = o, in_ = ass'}) =<< ass
 
 assignmentFromPred :: Pred Var -> Pred Val -> Resolution
-assignmentFromPred (Pred pred') (Pred poss)
-  | M.keysSet pred' /= M.keysSet poss = Error $
-    PredicatesOfDifferentShapes { requirement = Pred pred', possible_solution = Pred poss}
+assignmentFromPred a@(Pred pred') b@(Pred poss)
+  | M.keysSet pred' /= M.keysSet poss = (\ass''' -> Error $ PredicatesOfDifferentShapes { requirement = a, possible_solution = b, in_ = ass'''}) =<< ass''
   | otherwise = ass''
   where
     ass'' = foldr (uncurry restrictAtoms) mempty ass'
