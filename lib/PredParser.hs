c45be682ec707ea07e838c6d26a0bae4c536b154
Joshua Pratt <jp10010101010000@gmail.com>
2019-01-21 07:04:31 +1100

    Add parser for basic function based language

--- lib/PredParser.hs
+++ lib/PredParser.hs
@@ -1,21 +1,39 @@
 module PredParser where
-
-import Pred
+import Debug.Trace (trace)
+-- import Pred
 
 import Text.Parsec
-import Data.List (elemIndex)
+-- import Data.List (elemIndex)
+
+type Name = String
+type Path = String
 
 data PTerm
-  = TmVar Info Int Int
-  | TmAbs Info String PTerm
-  | TmApp Info PTerm PTerm
+  = TmFuncDef Info PFunc
+  | TmFuncCall Info PTerm Scope
+  | TmVar Info String
+  deriving Show
+
+type Scope = [(Name, PTerm)]
+
+data PFunc = PFunc
+  { name::Name
+  , info::Info
+  , args::[PTerm]
+  , pre::[PTerm]
+  , post::[PTerm]
+  , invar::[PTerm] -- maybe should be called static?
+  , defs::Scope -- internal scope
+  } deriving Show
+
+data PModule = PModule Name Path Scope
 
 data Info = Info
   { line :: Int
   , col :: Int
   } deriving (Show, Eq)
 
-type BoundContext = [String]
+type BoundContext = [Name]
 
 infoFrom :: SourcePos -> Info
 infoFrom pos = Info
@@ -23,63 +41,121 @@ infoFrom pos = Info
   , col = sourceColumn pos
   }
 
+getInfo :: Parsec String u Info
+getInfo = infoFrom <$> getPosition
+
+openParen :: Parsec String u ()
+openParen = do
+  _ <- char '('
+  _ <- many space
+  return ()
+
+closeParen :: Parsec String u ()
+closeParen = do
+  _ <- many space
+  _ <- char ')'
+  return ()
+
+withWhiteSpace :: Parsec String u r -> Parsec String u r
+withWhiteSpace exp = do
+  r <- exp
+  _ <- many $ char ' '<|>char '\n'
+  return r
+
 type PParser = Parsec String BoundContext PTerm
+
+newlineIndent :: Parsec String u ()
+newlineIndent = do
+  _ <- char '\n'
+  _ <- char ' '
+  _ <- char ' '
+  return ()
 
 parseVarName :: Parsec String u String
 parseVarName = do
-  h <- letter
-  ts <- many $ letter <|> digit
+  h <- trace ">> parseVarName" letter
+  ts <- many $ letter <|> digit <|> char '_'
   return (h:ts)
-
-parseAbs :: PParser -> PParser
-parseAbs termParser = do
-  _ <- char '\\'
-  v <- parseVarName
-  modifyState (v:)
-  _ <- char '.'
-  term <- termParser
-  modifyState tail
-  pos <- getPosition
-  return $ TmAbs (infoFrom pos) v term
 
 parseVar :: PParser
 parseVar = do
-  v <- parseVarName
-  list <- getState
-  findVar v list
+  name <- label parseVarName "variable name (e.g. foo1)"
+  pos <- getInfo
+  return $ TmVar pos name
 
-findVar :: String -> BoundContext -> PParser
-findVar v list
-  = case elemIndex v list of
-      Nothing -> fail $ "The variable '" ++ v ++ "' has not been bound"
-      Just n -> do
-        pos <- getPosition
-        return $ TmVar (infoFrom pos) n (length list)
+parseFuncDefArguments :: Parsec String BoundContext [PTerm]
+parseFuncDefArguments = sepBy parseVar (withWhiteSpace (char ','))
 
-parens :: Parsec String u a -> Parsec String u a
-parens = between (char '(') (char ')')
+parseAssignment :: Parsec String BoundContext (Name, PTerm)
+parseAssignment = do
+  name' <- parseVarName
+  _ <- many space
+  _ <- char '='
+  _ <- many space
+  term' <- parseTerm
+  return (name', term')
 
-parseNonApp :: PParser
-parseNonApp
-  = parens parseTerm -- (M)
-  <|> parseAbs parseTerm -- $\lambda $x.M
-  <|> parseVar
+parseFuncCallArguments :: Parsec String BoundContext [(Name, PTerm)]
+parseFuncCallArguments = trace ">> parseFuncCallArguments" $
+  sepBy parseAssignment $ withWhiteSpace $ char ','
+
+parseFuncCall :: PParser
+parseFuncCall = do
+  name' <- try $ do
+    name'' <- trace ">> parseFuncCall" parseVar
+    _ <- openParen
+    return name''
+  args' <- parseFuncCallArguments
+  _ <- closeParen
+  pos <- getInfo
+  return $ TmFuncCall pos name' args'
+
+
+parseFuncAssignment :: Parsec String BoundContext (Name, PTerm)
+parseFuncAssignment = do
+    _ <- try newlineIndent
+    parseAssignment
+
+parseFuncDef :: Parsec String BoundContext PFunc
+parseFuncDef = do
+  name' <- try $ do
+    name'' <- parseVarName
+    _ <- openParen
+    return name''
+  args' <- label parseFuncDefArguments "'(<arg> = <term>)'"
+  _ <- closeParen
+  pos <- getInfo
+  defs' <- many parseFuncAssignment
+  _ <- many space
+  return $ PFunc { name = name'
+                , info = pos
+                , args = args'
+                , pre = []
+                , post = []
+                , invar = []
+                , defs = defs'
+                }
+
+parseFuncDefTerm :: PParser
+parseFuncDefTerm = do
+  func' <- trace ">> parseFuncDefTerm" parseFuncDef
+  return $ TmFuncDef (info func') func'
 
 parseTerm :: PParser
 parseTerm
-  = chainl1 parseNonApp $ do
-    _ <- space
-    pos <- getPosition
-    return $ TmApp (infoFrom pos)
+  = parseFuncCall -- func(foo = var, bar = sum(a))
+  <|> parseFuncDefTerm -- func(foo, bar, baz)\n\tbaz=foo+bar
+  <|> parseVar
 
-parseLine :: PParser
-parseLine = do
-  term' <- parseTerm
-  _ <- char ';'
-  return term'
+
+parseModule :: Parsec String [Name] [PFunc]
+parseModule = do
+  defs <- many parseFuncDef
+  _ <- eof
+  return defs
 
 parseWith :: Parsec String [u] a -> String -> Either ParseError a
 parseWith p = runParser p [] "Predicate Parser"
 
-parse :: String -> Either ParseError PTerm
-parse = parseWith parseLine
+parse :: String -> Either ParseError [PFunc]
+parse = parseWith parseModule
