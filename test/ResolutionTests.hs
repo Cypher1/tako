70fbe13f4da2d508cae17997590e6ceaedf905db
Joshua Pratt <jp10010101010000@gmail.com>
2019-02-15 08:10:54 +1100

    Add in extra $ signs to ensure that long lines typecheck

--- test/ResolutionTests.hs
+++ test/ResolutionTests.hs
@@ -59,39 +59,39 @@ resolutionTests = testGroup "Resolution tests"
 
 emptyHeapletTests :: TestTree
 emptyHeapletTests = testGroup "Empty heaplet tests"
-  [ testCase "Value resolution fails if there is no heap"
+  [ testCase "Value resolution fails if there is no heap" $
       hasNoSolution $ solutions S.empty $ S.fromList [exists a]
   ]
 
 singlePredicateResolution :: TestTree
 singlePredicateResolution = testGroup "Single Predicate Resolution"
-  [ testCase "Value resolution passes if the state contains the value"
+  [ testCase "Value resolution passes if the state contains the value" $
       hasEmptySolution
     $ solutions (S.fromList [exists a]) $ S.fromList [exists a]
-  , testCase "Resolution succeeds if state contains the predicate"
+  , testCase "Resolution succeeds if state contains the predicate" $
       hasEmptySolution
       $ solutions (S.fromList [aNeZero]) $ S.fromList [aNeZero]
-  , testCase "Resolution fails on 1-pred with variable (no matches)"
+  , testCase "Resolution fails on 1-pred with variable (no matches)" $
       hasNoSolution
       $ solutions (S.fromList [exists a, exists ne, exists zero])
         $ S.fromList [varXNeZero]
-  , testCase "Resolution succeeds on 1-pred (with matches)"
+  , testCase "Resolution succeeds on 1-pred (with matches)" $
       hasSingleSolution [(x, a)]
       $ solutions (S.fromList [exists a, aNeZero])
         $ S.fromList [varXNeZero]
-  , testCase "Resolution correct on 1-pred (with alternate matches)"
+  , testCase "Resolution correct on 1-pred (with alternate matches)" $
       hasSingleSolution [(x, a)]
       $ solutions (S.fromList [exists a, exists b, aNeZero])
         $ S.fromList [varXNeZero]
-  , testCase "Resolution correct on 1-pred with variable"
+  , testCase "Resolution correct on 1-pred with variable" $
       hasSingleSolution [(x, a), (y, b)]
       $ solutions (S.fromList [exists a, exists b, aNeb])
         $ S.fromList [xNeY]
-  , testCase "Resolution fails on 1-pred with variable (a)"
+  , testCase "Resolution fails on 1-pred with variable (a)" $
       hasNoSolution
       $ solutions (S.fromList [exists a, aNeb])
         $ S.fromList [exists x, exists y, xNeY]
-  , testCase "Resolution fails on 1-pred with variable (b)"
+  , testCase "Resolution fails on 1-pred with variable (b)" $
       hasNoSolution
       $ solutions (S.fromList [exists b, aNeb])
         $ S.fromList [exists x, exists y, xNeY]
@@ -101,15 +101,15 @@ nestedPredicateTests :: TestTree
 nestedPredicateTests = testGroup "Nested predicate matching tests"
   --TODO(jopra): More tests for pattern matching (nested predicates)
   --TODO(jopra): Add tests for joins inside deeply nested predicates
-  [ testCase "Resolution fails on nested 1-pred without match"
+  [ testCase "Resolution fails on nested 1-pred without match" $
       hasNoSolution
       $ solutions (S.fromList [pred3 ne (Predicate $pred3 cons a b) list])
         $ S.fromList [pred3 isa x list]
-  , testCase "Resolution correct on pattern matched nested 1-pred"
+  , testCase "Resolution correct on pattern matched nested 1-pred" $
       hasSingleSolution [(x, a), (y, b)]
       $ solutions (S.fromList [pred3 isa (Predicate $ pred3 cons a b) list])
         $ S.fromList [pred3 isa (Predicate $ pred3 cons x y) list]
-  , testCase "Resolution correct on nested 1-pred"
+  , testCase "Resolution correct on nested 1-pred" $
       hasSingleSolution
       [(var "x", Predicate $ toPred [("#0", a), ("rel", cons), ("#1", b)])]
       $ solutions (S.fromList [pred3 isa (Predicate $pred3 cons a b) list])
@@ -118,15 +118,15 @@ nestedPredicateTests = testGroup "Nested predicate matching tests"
 
 joinResolutionTests :: TestTree
 joinResolutionTests = testGroup "Join resolution tests"
-  [ testCase "Resolution fails on 2-pred with variable"
+  [ testCase "Resolution fails on 2-pred with variable" $
       hasNoSolution
       $ solutions (S.fromList [isa' b c])
         $ S.fromList [pred3 isa x y, pred3 isa y z]
-  , testCase "Resolution fails on 2-pred with variable"
+  , testCase "Resolution fails on 2-pred with variable" $
       hasNoSolution
       $ solutions (S.fromList [pred3 isa a b])
         $ S.fromList [pred3 isa x y, pred3 isa y z]
-  , testCase "Resolution correct on 2-pred with variable"
+  , testCase "Resolution correct on 2-pred with variable" $
       hasSingleSolution [(z, c), (x, a), (y, b)]
       $ solutions (S.fromList [pred3 isa a b, pred3 isa b c])
         $ S.fromList [pred3 isa x y, pred3 isa y z]
