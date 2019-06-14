7923eee6263b9d1cf01629ef36b79fcf19099d82
Joshua Pratt <jp10010101010000@gmail.com>
2019-06-14 17:36:52 +1000

    Report line and column for messages

--- src/main.cc
+++ src/main.cc
@@ -1,22 +1,31 @@
 #include <iostream>
+#include <algorithm>
 #include <fstream>
 #include <sstream> //std::stringstream
 
 #include "ast.h"
 
-std::string toString(Token tok, const std::string contents) {
+std::string toString(const Location& loc, const std::string contents) {
   std::stringstream o;
-  o << tok.type << "@" << tok.loc.start << "->" << tok.loc.length << ": " << contents.substr(tok.loc.start, tok.loc.length);
+  size_t line = 1+std::count(contents.begin(), contents.begin()+loc.start, '\n');
+  size_t col = loc.start - contents.rfind("\n", loc.start);
+  o << "line " << line << " column " << col << "->" << loc.length << ": " << contents.substr(loc.start, loc.length);
   return o.str();
 }
 
-std::string toString(Message msg) {
+std::string toString(const Token& tok, const std::string& contents) {
   std::stringstream o;
-  o << msg.type << "@" << msg.loc.start << "->" << msg.loc.length << ": " << msg.msg;
+  o << tok.type << "@" << toString(tok.loc, contents);
   return o.str();
 }
 
-std::string toString(Tree<Token> tree, std::string contents, int depth=0) {
+std::string toString(const Message& msg, const std::string& contents) {
+  std::stringstream o;
+  o << msg.type << "@" << toString(msg.loc, contents) << ": " << msg.msg;
+  return o.str();
+}
+
+std::string toString(const Tree<Token>& tree, const std::string& contents, int depth=0) {
   std::stringstream o;
   for(int i=0; i<depth; i++) {
     o << "  ";
@@ -44,7 +53,7 @@ void runParser(std::string filename) {
   std::cout << toString(tree.value, contents) << "\n";
   std::cout << "Errors:\n";
   for(const auto msg : toks.msgs) {
-    std::cout << toString(msg) << "\n";
+    std::cout << toString(msg, contents) << "\n";
   }
 }
 
