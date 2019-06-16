b9d5a2c612405b70ecc30e4e1942538b8c03ff8d
Joshua Pratt <jp10010101010000@gmail.com>
2019-06-16 21:58:12 +1000

    Use c++14

--- src/parser/parser.cc
+++ src/parser/parser.cc
@@ -2,41 +2,43 @@
 #include <vector>
 #include <map>
 #include <string>
-#include <optional>
 
 #include "ast.h"
 #include "parser.h"
 #include "toString.h"
 
-std::optional<Value> parseValue( std::vector<Tree<Token>>::iterator& it, const std::vector<Tree<Token>>::iterator& end, Messages& msgs, const std::string& content, const std::string& filename) {
+Value parseValue( std::vector<Tree<Token>>::iterator& it, const std::vector<Tree<Token>>::iterator& end, Messages& msgs, const std::string& content, const std::string& filename) {
   const auto& node = *it;
+  const auto& loc = node.value.loc;
+  Value val = {
+    content.substr(loc.start, loc.length),
+    {},
+    node
+  };
   if(node.value.type == +TokenType::Symbol) {
-    const auto& loc = node.value.loc;
-    Value val = {
-      content.substr(loc.start, loc.length),
-      {},
-      node
-    };
     ++it;
     if(it != end && it->value.type == +TokenType::OpenParen) {
       val.args = it->children;
       ++it;
     }
     if(it == end) {
-      //TODO: needed definition
-      return std::nullopt;
+      msgs.push_back({
+          MessageType::Error,
+          "Needed definition ?",
+          it->value.type
+      });
+      return val;
     }
     val.def = *it;
     ++it;
     return val;
   }
-  //TODO: Unparseable.
   msgs.push_back({
       MessageType::Error,
       "Unexpected ?",
       it->value.type
   });
-  return std::nullopt;
+  return val;
 }
 
 Result<Module> parse(Result<Tree<Token>>& tree, const std::string& content, const std::string& filename) {
@@ -44,10 +46,9 @@ Result<Module> parse(Result<Tree<Token>>& tree, const std::string& content, cons
   std::vector<Value> values;
   auto children = tree.value.children;
   for(auto it = children.begin(); it != children.end(); ++it) {
-    std::optional<Value> val = parseValue(it, children.end(), msgs, content, filename);
-    if(val) {
-      values.push_back(*val);
-    }
+    values.push_back(
+      parseValue(it, children.end(), msgs, content, filename)
+    );
   }
   return {{filename, values}, msgs};
 }
