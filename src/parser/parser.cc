1e487067701429a8bff89d36eaca5116d2a39d78
Joshua Pratt <jp10010101010000@gmail.com>
2019-06-16 14:57:18 +1000

    Begin conversion from ast to functions

--- src/parser/parser.cc
+++ src/parser/parser.cc
@@ -8,14 +8,17 @@
 
 Result<Module> parse(Result<Tree<Token>>& tree, const std::string& content, const std::string& filename) {
   Messages& msgs = tree.msgs;
-  std::vector<std::string> values;
-  Value val;
+  std::vector<Value> values;
   for(const auto& node : tree.value.children) {
     if(node.value.type == +TokenType::Symbol) {
       const auto& loc = node.value.loc;
-      val.name = content.substr(loc.start, loc.length);
-      // std::cout << toString(node, content, filename, 0);
+      Value val = {
+        content.substr(loc.start, loc.length),
+        {},
+        node
+      };
       values.push_back(val);
+      // std::cout << toString(node, content, filename, 0);
     }
   }
   return {{filename, values}, msgs};
