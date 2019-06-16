56df1d7ba7baa8a8630b26045c4549a92d505955
Joshua Pratt <jp10010101010000@gmail.com>
2019-06-16 15:35:49 +1000

    Break out the value parser

--- src/parser/parser.cc
+++ src/parser/parser.cc
@@ -2,23 +2,40 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <optional>
+
 #include "ast.h"
 #include "parser.h"
 #include "toString.h"
 
+std::optional<Value> parseValue(
+std::vector<Tree<Token>>::iterator& it, Messages& msgs, const std::string& content, const std::string& filename) {
+  const auto& node = *it;
+  if(node.value.type == +TokenType::Symbol) {
+    const auto& loc = node.value.loc;
+    Value val = {
+      content.substr(loc.start, loc.length),
+      {},
+      node
+    };
+    return val;
+  }
+  //TODO: Unparseable.
+  msgs.push_back({
+      MessageType::Error,
+      "Unexpected ?",
+      it->value.type
+  });
+  return std::nullopt;
+}
+
 Result<Module> parse(Result<Tree<Token>>& tree, const std::string& content, const std::string& filename) {
   Messages& msgs = tree.msgs;
   std::vector<Value> values;
-  for(const auto& node : tree.value.children) {
-    if(node.value.type == +TokenType::Symbol) {
-      const auto& loc = node.value.loc;
-      Value val = {
-        content.substr(loc.start, loc.length),
-        {},
-        node
-      };
-      values.push_back(val);
-      // std::cout << toString(node, content, filename, 0);
+  for(auto it = tree.value.children.begin(); it != tree.value.children.end(); ++it) {
+    std::optional<Value> val = parseValue(it, msgs, content, filename);
+    if(val) {
+      values.push_back(*val);
     }
   }
   return {{filename, values}, msgs};
