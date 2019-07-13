76769d9a0b813c3d429ec74c0353d1e8467d698c
Joshua Pratt <jp10010101010000@gmail.com>
2019-07-13 11:50:33 +1000

    More lexer tests

--- test/parser_tests.cc
+++ test/parser_tests.cc
@@ -9,7 +9,6 @@
 
 TEST_CASE("empty string yields no tokens") {
   Messages msgs;
-  Config config;
   Context ctx = {msgs, "", "<filename>"};
 
   Tokens toks = lex(ctx);
@@ -19,7 +18,6 @@ TEST_CASE("empty string yields no tokens") {
 
 TEST_CASE("non-empty string yields tokens") {
   Messages msgs;
-  Config config;
   Context ctx = {msgs, " ", "<filename>"};
 
   Tokens toks = lex(ctx);
@@ -29,11 +27,64 @@ TEST_CASE("non-empty string yields tokens") {
 
 TEST_CASE("can lex a numeric literal") {
   Messages msgs;
-  Config config;
   Context ctx = {msgs, "12", "<filename>"};
 
   Tokens toks = lex(ctx);
-  CHECK_FALSE(toks.empty());
+  CHECK(toks.size() == 1);
+  CHECK(toks[0].type == +TokenType::NumberLiteral);
   CHECK(msgs.empty());
 }
 
+TEST_CASE("can lex a variable name") {
+  Messages msgs;
+  Context ctx = {msgs, "var", "<filename>"};
+
+  Tokens toks = lex(ctx);
+  CHECK(toks.size() == 1);
+  CHECK(toks[0].type == +TokenType::Symbol);
+  CHECK(msgs.empty());
+}
+
+TEST_CASE("can tokenize simple expressions") {
+  Messages msgs;
+  Context ctx = {msgs, "var * 32", "<filename>"};
+
+  Tokens toks = lex(ctx);
+  CHECK(toks.size() == 5);
+  CHECK(toks[0].type == +TokenType::Symbol);
+  CHECK(toks[1].type == +TokenType::WhiteSpace);
+  CHECK(toks[2].type == +TokenType::Operator);
+  CHECK(toks[3].type == +TokenType::WhiteSpace);
+  CHECK(toks[4].type == +TokenType::NumberLiteral);
+  CHECK(msgs.empty());
+}
+
+TEST_CASE("can tokenize simple expressions") {
+  Messages msgs;
+  Context ctx = {msgs, "32 * var", "<filename>"};
+
+  Tokens toks = lex(ctx);
+  CHECK(toks.size() == 5);
+  CHECK(toks[0].type == +TokenType::NumberLiteral);
+  CHECK(toks[1].type == +TokenType::WhiteSpace);
+  CHECK(toks[2].type == +TokenType::Operator);
+  CHECK(toks[3].type == +TokenType::WhiteSpace);
+  CHECK(toks[4].type == +TokenType::Symbol);
+  CHECK(msgs.empty());
+}
+
+TEST_CASE("can tokenize simple expressions") {
+  Messages msgs;
+  Context ctx = {msgs, "32 * var()", "<filename>"};
+
+  Tokens toks = lex(ctx);
+  CHECK(toks.size() == 7);
+  CHECK(toks[0].type == +TokenType::NumberLiteral);
+  CHECK(toks[1].type == +TokenType::WhiteSpace);
+  CHECK(toks[2].type == +TokenType::Operator);
+  CHECK(toks[3].type == +TokenType::WhiteSpace);
+  CHECK(toks[4].type == +TokenType::Symbol);
+  CHECK(toks[5].type == +TokenType::OpenParen);
+  CHECK(toks[6].type == +TokenType::CloseParen);
+  CHECK(msgs.empty());
+}
