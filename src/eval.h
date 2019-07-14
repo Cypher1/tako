462b7f92121888fd9c8ea430292d214d016b2a36
Joshua Pratt <jp10010101010000@gmail.com>
2019-07-14 14:26:17 +1000

    Lexer tests, interpreter stub and build cleanup (#117)
    * Create a interpreter stub
    
    * Cleanup some meson config
    
    * More lexer tests
    
    * More tests

---
+++ src/eval.h
@@ -0,0 +1,13 @@
+#ifndef EVAL_H
+#define EVAL_H
+
+#include <variant>
+#include <optional>
+#include <vector>
+#include <string>
+
+#include "enums.h"
+#include "util.h"
+#include "context.h"
+
+#endif // #ifndef EVAL_H
