45f436dc2e7cd98b450683a3cb3e8fcfb991bf72
Joshua Pratt <jp10010101010000@gmail.com>
2019-12-06 08:39:27 +1100

    Builder script

--- wasm/mybuild.sh
+++ wasm/mybuild.sh
@@ -1,8 +1,9 @@
 mkdir -p build
 mkdir -p gen
 
+cargo run "$1" > gen/myAddTwo.wat
 cp ./wabt/wasm2c/wasm-rt.h ./gen/wasm-rt.h
-./wabt/build/wat2wasm myAddTwo.wat -o gen/myAddTwo.wasm
+./wabt/build/wat2wasm gen/myAddTwo.wat -o gen/myAddTwo.wasm
 ./wabt/build/wasm2c gen/myAddTwo.wasm -o myAddTwo.c
 mv myAddTwo.c gen/
 mv myAddTwo.h gen/
