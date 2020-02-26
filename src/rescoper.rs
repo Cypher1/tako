57683c7abfe44753502ecebcdd0b6febdb2df51c
Joshua Pratt <jp10010101010000@gmail.com>
2020-02-26 19:35:10 +1100

    Fix for argument passing bug

--- src/rescoper.rs
+++ src/rescoper.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 #[derive(Debug, PartialEq)]
 pub enum ReScoperError {
     FailedParse(String, Info),
+    FailedSymbolLookup(String, Info),
 }
 
 // Walks the AST interpreting it.
@@ -24,6 +25,7 @@ impl Default for ReScoper {
 // TODO: Return nodes.
 type Res = Result<Node, ReScoperError>;
 
+#[derive(Debug)]
 pub struct Namespace {
     name: ScopeName,
     info: Definition,
@@ -56,12 +58,15 @@ impl Visitor<State, Node, Root, ReScoperError> for ReScoper {
             counter: 1,
         };
         let mut res = self.visit(&mut state, &expr.ast)?.to_root();
-        // Check requires
-        if !state.requires.is_empty() {
-            eprintln!("{:?} not declared", state.requires);
-        }
         // TODO(cypher1): Avoid this copy (use swaps?)
         res.graph = self.graph.clone();
+        // Check requires
+        if !state.requires.is_empty() {
+            return Err(ReScoperError::FailedSymbolLookup(
+                format!("{:?} not declared", state.requires),
+                expr.ast.get_info(),
+            ))
+        }
         Ok(res)
     }
 
@@ -128,50 +133,46 @@ impl Visitor<State, Node, Root, ReScoperError> for ReScoper {
         for namespace in state.stack.iter() {
             space.push(namespace.name.clone());
         }
+
+        if self.debug > 1 {
+            eprintln!("visiting {:?}", space.clone());
+        }
+
         {
             let frame = state.stack.last_mut().unwrap();
+            // Let this node and its siblings call this node.
             frame.info.defines.insert(expr.to_sym(), space.clone());
         }
 
+        // Find new graph node
+        let mut node = Definition {
+            defines: HashMap::new(),
+            requires: vec![],
+        };
+
+        // Consider the function arguments defined in this scope.
+        for arg in expr.args.clone().unwrap_or(vec![]) {
+            node.defines.insert(arg, space.clone());
+        }
+
+        // Push the scope onto the stack.
         state.stack.push(Namespace {
             name: ScopeName::Named(expr.name.clone(), state.counter),
-            info: Definition {
-                defines: HashMap::new(),
-                requires: expr.args.clone().unwrap_or(vec![]),
-            }
+            info: node,
         });
         state.counter += 1;
 
-        // Find new graph node
-        let mut node = Definition {
-            requires: vec![],
-            defines: HashMap::new(),
-        };
-        std::mem::swap(&mut node.requires, &mut state.requires);
+        // Examine the body of the let binding.
         let value = Box::new(self.visit(state, &expr.value)?);
-        std::mem::swap(&mut node.requires, &mut state.requires);
 
-        for req in state.requires.iter() {
-            if !node.requires.iter().any(|r| r.name == req.name) {
-                node.requires.push(req.clone());
-            }
-        }
-
-        // Now that the variable has been defined we can use it.
-        for req in node.requires.iter() {
-            if !state.requires.iter().any(|r| r.name == req.name) {
-                state.requires.push(req.clone());
-            }
-        }
         // Now that the variable has been defined we can use it.
         // if !recursive // frame.push(expr.name.clone());
 
-        self.graph.insert(space.clone(), node.clone());
-        if self.debug > 1 {
-            eprintln!("visiting {:?}", space);
-        }
+        // Finish the scope. Retrieve any information from.the stack.
+        node = state.stack.pop().unwrap().info;
 
-        state.stack.pop();
+        self.graph.insert(space.clone(), node.clone());
+
         let mut info = expr.get_info();
         info.defined_at = Some(space);
 
@@ -196,8 +197,36 @@ impl Visitor<State, Node, Root, ReScoperError> for ReScoper {
     }
 
     fn visit_bin_op(&mut self, state: &mut State, expr: &BinOp) -> Res {
+        // Find new graph node
+        let mut left_node = Definition {
+            requires: vec![],
+            defines: HashMap::new(),
+        };
+        std::mem::swap(&mut left_node.requires, &mut state.requires);
         let left = Box::new(self.visit(state, &expr.left)?);
+        std::mem::swap(&mut left_node.requires, &mut state.requires);
+
+        // Find new graph node
+        let mut right_node = Definition {
+            requires: vec![],
+            defines: HashMap::new(),
+        };
+        std::mem::swap(&mut right_node.requires, &mut state.requires);
         let right = Box::new(self.visit(state, &expr.right)?);
+        std::mem::swap(&mut right_node.requires, &mut state.requires);
+
+        for req in right_node.requires.iter() {
+            if expr.name != "?" || left_node.requires.iter().any(|l| l.name == req.name) {
+                state.requires.push(req.clone());
+            }
+        }
+        for req in left_node.requires.iter() {
+            if expr.name != "?" || right_node.requires.iter().any(|l| l.name == req.name) {
+                state.requires.push(req.clone());
+            }
+        }
+
+
         Ok(BinOp {
             name: expr.name.clone(),
             left,
