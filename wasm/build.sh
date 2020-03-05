a0b629397df92641dcc0c675f3d849016648257f
Joshua Pratt <jp10010101010000@gmail.com>
2020-03-06 01:10:44 +1100

    Bug fix for build tests

--- wasm/build.sh
+++ wasm/build.sh
@@ -74,7 +74,7 @@ require "header from wabt" "gen/wasm-rt.h" || \
   cp wabt/wasm2c/wasm-rt.h gen/wasm-rt.h
 
 # Run tako's core.
-realt tako "$1"
+realt tako --wasm "$1"
 # Generate our C files ('compile to C')
 realt wat2c "addTwo"
 
