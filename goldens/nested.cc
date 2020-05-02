d1b933187b2a7d45511676f74bc0d5491aaccca5
Joshua Pratt <jp10010101010000@gmail.com>
2020-05-02 21:05:39 +1000

    Update goldens enforcing expected formatting (#224)

--- goldens/nested.cc
+++ goldens/nested.cc
@@ -1,10 +1,6 @@
 
 int main(int argc, char* argv[]) {
-  
-    const int x_y = 3;
-
-  
-    const int x = (x_y*2);
-
+  const int x_y = 3;
+  const int x = (x_y*2);
   return (x*5);
 };
--- goldens/three_vars.cc
+++ goldens/three_vars.cc
@@ -1,13 +1,7 @@
 
 int main(int argc, char* argv[]) {
-  
-    const int x = 2;
-
-  
-    const int y = 3;
-
-  
-    const int z = (x+y);
-
+  const int x = 2;
+  const int y = 3;
+  const int z = (x+y);
   return ((x*y)*z);
 };
