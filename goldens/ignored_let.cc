2485c01b98210bd2053f64b3949f016ff28ddc7f
Joshua Pratt <jp10010101010000@gmail.com>
2020-05-02 15:12:15 +1000

    Remove broken goldens

--- goldens/ignored_let.cc
+++
@@ -1,6 +0,0 @@
-
-int main(int argc, char* argv[]) {
-  
-    const int x = 4;
-  return 1;
-};
--- goldens/multiple_assignment.cc
+++
@@ -1,6 +0,0 @@
-
-int main(int argc, char* argv[]) {
-  
-    const int x = const int x_y = 3;;
-  return (x*5);
-};
