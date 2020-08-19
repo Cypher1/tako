a5fa55832c84b667588626a009f1535ead23ab8e
Joshua Pratt <jp10010101010000@gmail.com>
2020-08-20 00:10:21 +1000

    Add more type tests

--- src/type_checker.rs
+++ src/type_checker.rs
@@ -6,6 +6,7 @@ use crate::errors::TError;
 pub fn infer(db: &dyn Compiler, expr: Node) -> Result<Node, TError> {
     // Infer that expression t has type A, t => A
     // See https://ncatlab.org/nlab/show/bidirectional+typechecking
+    use crate::ast::*;
     match expr {
         PrimNode(prim) => match prim {
             I32(_, _) => Ok(Sym {
@@ -25,7 +26,24 @@ pub fn infer(db: &dyn Compiler, expr: Node) -> Result<Node, TError> {
             .to_node()),
             Lambda(node) => db.infer(*node), // TODO: abstraction
         },
-        _ => panic!(),
+        UnOpNode(UnOp{name: _, inner: _, info: _}) => {
+            panic!("TODO Impl type checking for UnOp")
+        }
+        BinOpNode(BinOp{name: _, left: _, right: _, info: _}) => {
+            panic!("TODO Impl type checking for BinOp")
+        }
+        SymNode(Sym{name: _, info: _}) => {
+            panic!("TODO Impl type checking for Sym")
+        }
+        ApplyNode(Apply{inner: _, args: _, info: _}) => {
+            panic!("TODO Impl type checking for Apply")
+        }
+        LetNode(Let{name: _, value: _, args: _, info: _}) => {
+            panic!("TODO Impl type checking for Let")
+        }
+        Error(Err{msg: _, info: _}) => {
+            panic!("TODO Impl type checking for Let")
+        }
     }
 }
 
@@ -36,17 +54,44 @@ mod tests {
     use crate::database::DB;
     use crate::parser::tests::parse_with_db;
 
-    #[test]
-    fn infer_type_of_i32() {
-        let db = DB::default();
-        let num = I32(23, Info::default()).to_node();
-        assert_eq!(infer(&db, num), Ok(Sym{name: "I32".to_owned(), info: Info::default()}.to_node()));
+    fn assert_type(prog: &str, ty: &str) {
+        let mut db = DB::default();
+        let prog = parse_with_db(&mut db, prog.to_string());
+        let prog = infer(&mut db, prog);
+        let ty = parse_with_db(&mut db, ty.to_string());
+        assert_eq!(prog, Ok(ty));
     }
 
-    // Disable for now #[test]
-    fn infer_type_of_plus_expr() {
+    #[test]
+    fn infer_type_of_i32() {
         let mut db = DB::default();
-        let num = parse_with_db(&mut db, "12+32".to_string());
-        assert_eq!(infer(&db, num), Ok(Sym{name: "I32".to_owned(), info: Info::default()}.to_node()));
+        let num = I32(23, Info::default()).to_node();
+        assert_eq!(infer(&mut db, num), Ok(Sym{name: "I32".to_owned(), info: Info::default()}.to_node()));
+        assert_type("23", "I32");
+    }
+
+    #[test]
+    fn infer_type_of_sym_i32() {
+        assert_type("x=12;x", "I32");
+    }
+
+    #[test]
+    fn infer_type_of_sym_with_extra_lets_i32() {
+        assert_type("x=12;y=4;x", "I32");
+    }
+
+    #[test]
+    fn infer_type_of_id_apply() {
+        assert_type("(x)(x=12)", "I32");
+    }
+
+    #[test]
+    fn infer_type_of_id_apply_it_arg() {
+        assert_type("(it)(12)", "I32");
+    }
+
+    #[test]
+    fn infer_type_of_plus_expr() {
+        assert_type("12+32", "I32");
     }
 }
