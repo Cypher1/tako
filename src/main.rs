c2d9a95db2bd892e23071c8cfa33e92c73ea268b
Joshua Pratt <jp10010101010000@gmail.com>
2020-10-11 22:57:22 +1100

    Add placeholder repl (with no parsing+execution)

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,24 @@
 use std::env;
 use std::error::Error;
 
+use rustyline::error::ReadlineError;
+use rustyline::{Editor, config::Config};
+use directories::ProjectDirs;
+
 use takolib::cli_options::Options;
 use takolib::database::{Compiler, DB};
 use takolib::errors::TError;
 use takolib::work;
 
 fn main() -> Result<(), TError> {
-    let args: Vec<String> = env::args().collect();
-
     let mut db = DB::default();
+
+    let args: Vec<String> = env::args().collect();
+    let project_dirs = ProjectDirs::from("systems", "mimir", "tako");
+    db.set_project_dirs(project_dirs);
+
+    std::fs::create_dir_all(&db.config_dir()).unwrap();
+
     db.set_options(Options::new(&args[1..]));
 
     for f in db.options().files.iter() {
@@ -25,5 +34,45 @@ fn main() -> Result<(), TError> {
             }
         }
     }
+
+    if db.options().interactive {
+        repl(&mut db)
+    } else {
+        Ok(())
+    }
+}
+
+fn repl(db: &mut DB) -> Result<(), TError> {
+    // `()` can be used when no completer is required
+    let rl_config = Config::builder()
+        .tab_stop(2)
+        .build();
+
+    let mut rl = Editor::<()>::with_config(rl_config);
+    if rl.load_history(&db.history_file()).is_err() {
+        println!("No previous history.");
+    }
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                println!("Line: {}", line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
+    rl.save_history(&db.history_file()).unwrap();
     Ok(())
 }
