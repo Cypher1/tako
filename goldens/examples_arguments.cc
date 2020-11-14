879d6fce69478b2cdf6b4803963334cce45d68aa
Joshua Pratt <jp10010101010000@gmail.com>
2020-11-14 14:35:50 +1100

    Indent lambda args

--- goldens/examples_arguments.cc
+++ goldens/examples_arguments.cc
@@ -1,6 +1,8 @@
 
 int main(int argc, char* argv[]) {
-  const auto examples_arguments_x = [&](const auto examples_arguments_x_y) {
+  const auto examples_arguments_x = [&](
+    const auto examples_arguments_x_y
+  ) {
     return (examples_arguments_x_y+1);
   };
   return examples_arguments_x(3);
