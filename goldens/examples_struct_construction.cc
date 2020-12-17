3b5f18f54204157732192ce325dfa54ae97862eb
Joshua Pratt <jp10010101010000@gmail.com>
2020-12-17 23:21:25 +1100

    Improve expectations for generated to_string and operator<< implementations

--- goldens/examples_struct_construction.cc
+++ goldens/examples_struct_construction.cc
@@ -8,16 +8,16 @@ typedef struct {
   int32_t y;
 } struct_x_i32_y_i32;
 
+std::ostream& operator<<(std::ostream& os, const struct_x_i32_y_i32& t) {
+  os << "{x=" << t.x << ", y=" << t.y << "}";
+  return os;
+}
+
 namespace std{
 template <typename T>
 string to_string(const T& t){
   stringstream out;
   out << t;
-  return out.str();
-}
-string to_string(const struct_x_i32_y_i32& t){
-  stringstream out;
-  out << "{x=" << to_string(t.x) << ", y=" << to_string(t.y) << "}";
   return out.str();
 }
 string to_string(const bool& t){
@@ -27,6 +27,6 @@ string to_string(const bool& t){
 
 int main(int argc, char* argv[]) {
   const struct_x_i32_y_i32 examples_struct_construction_ob = {.x=3, .y=5};
-  std::cout << ((std::to_string("ob=")+std::to_string(examples_struct_construction_ob))) << "\n";
+  std::cout << ((std::to_string("ob=")+(std::to_string(examples_struct_construction_ob) + "\n")));
   return 0;
 }
