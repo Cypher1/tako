e6dea1c9dc7f9cc40a0826a0960578f63783b25f
Joshua Pratt <jp10010101010000@gmail.com>
2020-12-18 00:04:17 +1100

    Match golden to fit code gen quirks (without changing semantics)

--- goldens/examples_struct_construction.cc
+++ goldens/examples_struct_construction.cc
@@ -1,18 +1,6 @@
 #include <iostream>
-#include <stdlib.h>
 #include <string>
 #include <sstream>
-
-typedef struct {
-  int32_t x;
-  int32_t y;
-} struct_x_i32_y_i32;
-
-std::ostream& operator<<(std::ostream& os, const struct_x_i32_y_i32& t) {
-  os << "struct(x=" << t.x << ", y=" << t.y << ")";
-  return os;
-}
-
 namespace std{
 template <typename T>
 string to_string(const T& t){
@@ -24,9 +12,11 @@ string to_string(const bool& t){
   return t ? "true" : "false";
 }
 }
+typedef struct {int32_t x; int32_t y;} struct_x_i32_y_i32;
+std::ostream& operator<<(std::ostream& os, const struct_x_i32_y_i32& t) { os << "struct(x=" << t.x << ", y=" << t.y << ")"; return os; }
 
 int main(int argc, char* argv[]) {
-  const auto examples_struct_construction_ob = ([](const auto x, const auto y){return struct_x_i32_y_i32{.x=x,.y=y};})(3, 5);
-  std::cout << ((std::to_string("ob=")+(std::to_string(examples_struct_construction_ob) + "\n")));
+  const auto examples_struct_construction_ob = [](const auto x, const auto y){return struct_x_i32_y_i32{.x=x,.y=y};}(3, 5);
+  std::cout << ((std::to_string((std::to_string("ob=")+std::to_string(examples_struct_construction_ob)))+std::to_string("\n")));
   return 0;
 }
