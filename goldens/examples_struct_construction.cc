c03df38599c2d5472bbe5720230302f58d0719ff
Joshua Pratt <jp10010101010000@gmail.com>
2020-12-18 11:12:16 +1100

    Dont use auto for struct constructor args

--- goldens/examples_struct_construction.cc
+++ goldens/examples_struct_construction.cc
@@ -17,7 +17,7 @@ struct x_i32_y_i32 {int32_t x; int32_t y;};
 std::ostream& operator<<(std::ostream& os, const struct x_i32_y_i32& t) { os << "struct(x=" << t.x << ", y=" << t.y << ")"; return os; }
 
 int main(int argc, char* argv[]) {
-  const auto examples_struct_construction_ob = [](const auto x, const auto y){return x_i32_y_i32{.x=x,.y=y};}(3, 5);
+  const auto examples_struct_construction_ob = [](const int32_t x, const int32_t y){return x_i32_y_i32{.x=x,.y=y};}(3, 5);
   std::cout << ((std::to_string((std::to_string("ob=")+std::to_string(examples_struct_construction_ob)))+std::to_string("\n")));
   return 0;
 }
--- goldens/examples_struct_construction2.cc
+++ goldens/examples_struct_construction2.cc
@@ -17,7 +17,7 @@ struct x_i32_y_i32_z_i32{int32_t x; int32_t y;};
 std::ostream& operator<<(std::ostream& os, const struct x_i32_y_i32_z_i32& t) { os << "struct(x=" << t.x << ", y=" << t.y << ", z=" << t.z << ")"; return os; }
 
 int main(int argc, char* argv[]) {
-  const auto examples_struct_construction_ob = [](const auto x, const auto y, const auto z){return x_i32_y_i32_z_i32{.x=x,.y=y,.z=z};}(2, 1, 3);
+  const auto examples_struct_construction_ob = [](const int32_t x, const int32_t y, const int32_t z){return x_i32_y_i32_z_i32{.x=x,.y=y,.z=z};}(2, 1, 3);
   std::cout << ((std::to_string((std::to_string("h=")+std::to_string(examples_struct_construction_ob)))+std::to_string("\n")));
   return 0;
 }
