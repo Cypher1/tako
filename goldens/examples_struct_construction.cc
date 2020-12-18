18c6ebc580d07c51a5e42381069a865a0db3e486
Joshua Pratt <jp10010101010000@gmail.com>
2020-12-18 18:05:20 +1100

    Simplify struct construction

--- goldens/examples_struct_construction.cc
+++ goldens/examples_struct_construction.cc
@@ -13,11 +13,11 @@ string to_string(const bool& t){
 }
 }
 struct x_I32y_I32;
-struct x_I32y_I32 {int32_t x; int32_t y;};
+struct x_I32y_I32{int32_t x; int32_t y; x_I32y_I32(const int32_t x, const int32_t y): x{x}, y{y} {}};
 std::ostream& operator<<(std::ostream& os, const struct x_I32y_I32& t) { os << "struct(x=" << t.x << ", y=" << t.y << ")"; return os; }
 
 int main(int argc, char* argv[]) {
-  const auto examples_struct_construction_ob = [](const int32_t x, const int32_t y){return x_I32y_I32{.x=x,.y=y};}(3, 5);
+  const auto examples_struct_construction_ob = x_I32y_I32(3, 5);
   std::cout << ((std::to_string((std::to_string("ob=")+std::to_string(examples_struct_construction_ob)))+std::to_string("\n")));
   return 0;
 }
