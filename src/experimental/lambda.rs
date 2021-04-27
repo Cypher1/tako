79216d21d6cde544b6edaefaa709d1a11c19ee85
J Pratt <jp10010101010000@gmail.com>
2021-04-27 17:39:41 +1000

    Add lambda evaluation and tests

--- src/experimental/lambda.rs
+++ src/experimental/lambda.rs
@@ -11,19 +11,18 @@ pub enum Term {
 use Term::*;
 
 fn var(ind: Ind) -> Term {
-    Term::Var{ind}
+    Var{ind}
 }
 
 fn app(inner: Term, arg: Term) -> Term {
-    Term::App{inner: Box::new(inner), arg: Box::new(arg)}
+    App{inner: Box::new(inner), arg: Box::new(arg)}
 }
 
 fn abs(inner: Term) -> Term {
-    Term::Abs{inner: Box::new(inner)}
+    Abs{inner: Box::new(inner)}
 }
 
 impl Term {
-
     fn shift(&self, delta: Delta) -> Term {
         self.shift_with_cutoff(delta, 0)
     }
@@ -34,7 +33,6 @@ impl Term {
                 if *ind < cutoff {
                     self.clone()
                 } else {
-                    eprintln!("IND: {}, CUTOFF: {}, DELTA: {}", &ind, &cutoff, &delta);
                     var(ind.checked_add(delta).expect("Should not run out of indexes"))
                 }
             }
@@ -59,11 +57,65 @@ impl Term {
             Abs {inner} => abs(inner.substitute(x+1, &with.shift(1))),
         }
     }
+
+    fn beta_reduce(&self) -> Term {
+        if let App {inner, arg} = self {
+            let inner: &Term = &*inner; // TODO: Work out why I can't write this in one line.
+            if let Abs {inner} = inner {
+                return inner.substitute(0, &arg.shift(1)).shift(-1);
+            }
+        }
+        self.clone()
+    }
+
+    fn eval(&self) -> Term {
+        match self {
+            Var {..} => self.clone(),
+            App {inner, arg} => {
+                let simplified = app(inner.eval(), arg.eval());
+                let reduced = simplified.beta_reduce();
+                if reduced != simplified {
+                    return reduced.eval();
+                }
+                simplified
+            }
+            Abs {inner} => abs(inner.eval()),
+        }
+    }
+}
+
+mod util {
+    use super::*;
+
+    pub fn church_bool(b: bool) -> Term {
+        abs(abs(var(if b { 1 } else { 0 })))
+    }
+
+    pub fn church_not() -> Term {
+        abs(app(app(var(0), church_bool(false)), church_bool(true)))
+    }
+
+    pub fn church_nat(n: i32) -> Term {
+        let f = || var(1);
+        let x = || var(0);
+
+        let mut curr = x();
+        for _ in 0..n {
+            curr = app(f(), curr);
+        }
+        abs(abs(curr))
+    }
+
+    pub fn church_plus() -> Term {
+        //          m.  n.  f.  x.          ((m f)((n f) x))
+        abs(abs(abs(abs(app(app(var(3), var(1)), app(app(var(2), var(1)), var(0)))))))
+    }
 }
 
 mod test {
     use pretty_assertions::assert_eq;
     use super::*;
+    use super::util::*;
 
     #[test]
     fn basic_shift() {
@@ -107,5 +159,57 @@ mod test {
     #[test]
     fn substitute_x_app_x() {
         assert_eq!(app(var(0), var(0)).substitute(0, &abs(var(1))), app(abs(var(1)), abs(var(1))));
+    }
+
+    #[test]
+    fn basic_beta_reduction() {
+        assert_eq!(
+            app(abs(app(app(var(1), var(0)), var(2))), abs(var(0))).beta_reduce(),
+            app(app(var(0), abs(var(0))), var(1))
+        )
+    }
+
+    #[test]
+    fn eval_one_step() {
+        let id = abs(var(0));
+        let before = app(app(app(abs(abs(abs(app(app(var(1), var(2)), var(0))))), id.clone()), var(5)), var(6));
+        let after = app(app(var(5), id), var(6));
+        assert_eq!(before.eval(), after);
+    }
+
+    #[test]
+    fn eval_two_step() {
+        let id = abs(var(0));
+        let before = app(app(app(abs(abs(abs(app(app(var(1), var(2)), var(0))))), var(5)), id), var(6));
+        let after = app(var(5), var(6));
+        assert_eq!(before.eval(), after);
+    }
+
+    #[test]
+    fn eval_church_true() {
+        assert_eq!(app(app(church_bool(true), church_nat(5)), church_nat(6)).eval(), church_nat(5));
+    }
+
+    #[test]
+    fn eval_church_false() {
+        assert_eq!(app(app(church_bool(false), church_nat(5)), church_nat(6)).eval(), church_nat(6));
+    }
+
+    #[test]
+    fn eval_church_not_true() {
+        assert_eq!(app(church_not(), church_bool(true)).eval(), church_bool(false));
+    }
+
+    #[test]
+    fn eval_church_not_false() {
+        assert_eq!(app(church_not(), church_bool(false)).eval(), church_bool(true));
+    }
+
+    #[test]
+    fn eval_church_3_plus_4_eq_7() {
+        let three = church_nat(3);
+        let four = church_nat(4);
+        let seven = church_nat(7);
+        assert_eq!(app(app(church_plus(), three), four).eval(), seven);
     }
 }
