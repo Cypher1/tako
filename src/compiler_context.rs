96caa8516e2ee81389781211dec6dc06ff52d8d3
J Pratt <jp10010101010000@gmail.com>
2022-10-02 10:04:58 +1100

    tmp

--- src/compiler_context.rs
+++ src/compiler_context.rs
@@ -1,7 +1,8 @@
 use crate::cli_options::Options;
 use crate::compiler_tasks::JobTypes;
 use crate::concepts::*;
-use crate::free_standing::jobs::JobStore;
+use crate::free_standing::jobs::{JobStore, FinishType};
+use log::info;
 
 #[derive(Default, Debug)]
 pub struct CompilerContext {
@@ -22,5 +23,46 @@ impl CompilerContext {
             options,
             ..Self::default()
         }
+    }
+
+    fn plan_build_jobs(&mut self) {
+        todo!();
+    }
+
+    fn plan_pre_interpret_jobs(&mut self) -> JobId {
+        todo!();
+    }
+
+    fn plan_interpret_jobs(&mut self) {
+        let init_job_id = self.plan_pre_interpret_jobs();
+
+        todo!();
+    }
+    fn plan_repl_jobs(&mut self) {
+        let init_job_id = self.plan_pre_interpret_jobs();
+
+                for file in self.options.files {
+                    self.jobs.add_job();
+                }
+        todo!();
+    }
+    pub fn plan_jobs(&mut self) {
+        use crate::cli_options::Command;
+        match self.options.cmd {
+            Build => self.plan_build_jobs(),
+            Interpret => self.plan_interpret_jobs(),
+            Repl => self.plan_repl_jobs(),
+        }
+    }
+
+    pub fn run_job_loop(&mut self) {
+        while let Some((job_id, job)) = self.jobs.get_job() {
+            dbg!(&job_id, &job);
+
+            let result = FinishType::Success;
+            // Do some stuff
+            self.jobs.finish_job(job_id, result);
+        }
+        info!("All tasks completed.");
     }
 }
--- src/compiler_tasks.rs
+++ src/compiler_tasks.rs
@@ -2,6 +2,7 @@ use crate::concepts::*;
 
 #[derive(Debug, Copy, Clone)]
 pub enum JobTypes {
+    Load(String, FileId),
     Lex(FileId),
     Parse(FileId),
     TypeCheck(ModuleId),
