3ee4466764370a3e77787e440bbff09e7ed45dfc
J Pratt <jp10010101010000@gmail.com>
2022-10-27 00:00:27 +1100

    Some rough ideas about the direction to take

--- src/components.rs
+++ src/components.rs
@@ -9,45 +9,26 @@ use std::collections::BTreeSet;
 // TODO: Use macro for defining and registering each of these.
 #[derive(Component, Clone, Eq, PartialEq, PartialOrd, Ord, Hash)]
 #[storage(VecStorage)]
-pub struct InstancesAt(pub BTreeSet<Loc>);
-
-impl std::fmt::Debug for InstancesAt {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "InstancesAt(")?;
-        let mut after = false;
-        for item in &self.0 {
-            write!(f, "{:?}", item)?;
-            if after {
-                write!(f, ", ")?;
-            }
-            after = true;
-        }
-        write!(f, ")")
-    }
+pub struct File {
+    path: String, // TODO: Use something 'right'
+    root: Entity,
+    contents: String,
 }
 
-#[derive(Component, Clone, Debug, Eq, PartialEq, PartialOrd, Ord, Hash)]
+#[derive(Component, Clone, Eq, PartialEq, PartialOrd, Ord, Hash)]
 #[storage(VecStorage)]
-pub struct HasValue(pub Val);
-
-impl HasValue {
-    #[cfg(test)]
-    pub fn new<T: Into<Val>>(val: T) -> Self {
-        Self(val.into())
-    }
+pub struct Symbol {
+    location: u32, // index into the file
+    length: u8, // length of the symbol
+    file_id: TypedIndex<File>,
 }
 
-#[derive(Component, Clone, Default, Debug, Eq, PartialEq, PartialOrd, Ord, Hash)]
-#[storage(NullStorage)]
-pub struct Untyped;
-
-#[derive(Component, Clone, Debug, Eq, PartialEq, PartialOrd, Ord, Hash)]
+#[derive(Component, Clone, Eq, PartialEq, PartialOrd, Ord, Hash)]
 #[storage(VecStorage)]
-pub struct HasType(pub Entity);
-
-#[derive(Component, Clone, Debug, Eq, PartialEq, PartialOrd, Ord, Hash)]
-#[storage(VecStorage)]
-pub struct HasErrors(pub Vec<TError>);
+pub struct Location {
+    location: usize, // index into the file
+    file_id: TypedIndex<File>,
+}
 
 #[derive(Component, Clone, Debug, Eq, PartialEq, PartialOrd, Ord, Hash)]
 #[storage(VecStorage)]
@@ -66,10 +47,6 @@ impl Call {
     }
 }
 
-#[derive(Component, Clone, Debug, Eq, PartialEq, PartialOrd, Ord, Hash)]
-#[storage(VecStorage)]
-pub struct Sequence(pub Vec<Entity>); // TODO: Short vec
-
 #[derive(Component, Clone, Default, Debug, Eq, PartialEq, PartialOrd, Ord, Hash)]
 #[storage(VecStorage)]
 pub struct SymbolRef {
@@ -78,26 +55,9 @@ pub struct SymbolRef {
     pub definition: Option<Entity>,
 }
 
-impl SymbolRef {
-    #[cfg(test)]
-    pub fn new(name: Path, context: Path) -> Self {
-        Self {
-            name,
-            context,
-            definition: None,
-        }
-    }
-}
-
-#[derive(Component, Clone, Default, Debug, Eq, PartialEq, PartialOrd, Ord, Hash)]
-#[storage(NullStorage)]
-pub struct IsAst;
-
 #[derive(Component, Clone, Debug, Eq, PartialEq, PartialOrd, Ord, Hash)]
 #[storage(VecStorage)]
 pub struct Definition {
-    pub names: Vec<Path>,
-    pub params: Option<Vec<Entity>>,
-    pub implementations: Vec<Entity>,
-    pub path: Path,
+    pub name: TypedIndex<Symbol>,
+    pub implementation: Entity,
 }
---
+++ src/typed_index.rs
@@ -0,0 +1,30 @@
+use std::marker::PhantomData;
+
+struct TypedIndex<T, Index=u32> {
+    index: Index,
+    ty: PhantomData<T>,
+}
+
+impl<T, I: PartialEq> PartialEq<T> for TypedIndex<T, I> {
+    fn eq(&self, other: &Self) -> bool {
+        self.index == other.index
+    }
+}
+impl<T, I: Eq> Eq<T> for TypedIndex<T, I> {}
+
+impl<T, I: Clone> Clone<T> for TypedIndex<T, I> {
+    fn clone(&self) -> Self {
+        Self {
+            index: *self.index,
+            ty: PhantomData,
+        }
+    }
+}
+impl<T, I: Copy> Copy<T> for TypedIndex<T, I> {}
+
+impl<T, I: std::fmt::Debug> std::fmt::Debug for TypedIndex<T, I> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}_{}", std::any::type_name::<T>(), self.index)
+    }
+}
+
