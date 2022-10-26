ef206e5824c72722c6ea865f70d403398f1e124e
J Pratt <jp10010101010000@gmail.com>
2022-10-26 23:33:43 +1100

    Remove extras

--- benches/timing.sh
+++
@@ -1,1 +0,0 @@
-cargo test -- --test-threads 1 --report-time -Z unstable-options | sed "s/\(.*\)<\(.*\)>/\2  \1/" | sort -n
--- src/experimental/mod.rs
+++
@@ -1,4 +0,0 @@
-mod lambda;
-mod ski;
-mod type_graph;
-mod type_graph_builder;
