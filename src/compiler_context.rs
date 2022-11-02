2362872ac0f190472e1cb126ea12bb1456c90095
J Pratt <jp10010101010000@gmail.com>
2022-11-02 21:18:29 +1100

    TMP

--- src/compiler_context.rs
+++ src/compiler_context.rs
@@ -1,27 +1,41 @@
 use crate::cli_options::Options;
-use crate::compiler_tasks::JobTypes;
+use crate::compiler_tasks::JobTypes::{self, *};
 use crate::concepts::*;
-use crate::free_standing::jobs::{JobStore, FinishType};
+use crate::free_standing::jobs::{Job, JobId as BaseJobId, JobStore, FinishType};
+use crate::string_interner::StrInterner;
 use log::info;
 
+type JobId = BaseJobId<JobTypes>;
+
 #[derive(Default, Debug)]
-pub struct CompilerContext {
+pub struct CompilerStorage {
     files: FileVec,
     modules: ModuleVec,
     errors: ErrorVec,
     jobs: JobStore<JobTypes>,
-    options: Options,
 }
 
-impl CompilerContext {
-    pub fn new() -> Self {
-        Self::default()
-    }
+#[derive(Debug)]
+pub struct CompilerContext<'opts> {
+    store: CompilerStorage,
+    options: &'opts Options,
+}
 
-    pub fn from_options(options: Options) -> Self {
+impl<'opts> std::ops::Deref for CompilerContext<'opts> {
+    type Target = CompilerStorage;
+    fn deref(&self) -> &CompilerStorage { &self.store }
+}
+
+impl<'opts> std::ops::DerefMut for CompilerContext<'opts> {
+    // type Target = CompilerStorage;
+    fn deref_mut(&mut self) -> &mut CompilerStorage { &mut self.store }
+}
+
+impl<'opts> CompilerContext<'opts> {
+    pub fn new(options: &'opts Options) -> Self {
         Self {
             options,
-            ..Self::default()
+            store: CompilerStorage::default(),
         }
     }
 
@@ -29,25 +43,40 @@ impl CompilerContext {
         todo!();
     }
 
+    fn plan_parse_file(&mut self, path: String) -> JobId {
+        let fileid = FileId::new(&mut self.files, File {
+            path,
+            string_interner: StrInterner::new(),
+            root: None,
+            contents: None,
+            lexed: None,
+            ast: None,
+        });
+        let load_id = self.jobs.add_job(Job::new(Load(fileid), vec![]));
+        let lex_id = self.jobs.add_job(Job::new(Lex(fileid), vec![load_id]));
+        let parse_id = self.jobs.add_job(Job::new(Parse(fileid), vec![lex_id]));
+        parse_id
+    }
+
     fn plan_pre_interpret_jobs(&mut self) -> JobId {
-        todo!();
+        let mut prep_jobs = vec![];
+        for path in self.options.files {
+            let parse_id = self.plan_parse_file(path);
+            prep_jobs.push(parse_id);
+        }
+        self.jobs.add_job(Job::new(AllFilesParsed, prep_jobs))
     }
 
     fn plan_interpret_jobs(&mut self) {
         let init_job_id = self.plan_pre_interpret_jobs();
-
         todo!();
     }
     fn plan_repl_jobs(&mut self) {
         let init_job_id = self.plan_pre_interpret_jobs();
-
-                for file in self.options.files {
-                    self.jobs.add_job();
-                }
         todo!();
     }
     pub fn plan_jobs(&mut self) {
-        use crate::cli_options::Command;
+        // use crate::cli_options::Command;
         match self.options.cmd {
             Build => self.plan_build_jobs(),
             Interpret => self.plan_interpret_jobs(),
