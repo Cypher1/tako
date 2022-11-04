b0109a82d0ad44ac304b6f7893493d8a50b9b2fc
J Pratt <jp10010101010000@gmail.com>
2022-11-04 23:24:38 +1100

    Pretty print main's logs

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ async fn main() -> Result<()> {
 
     let args: Vec<String> = env::args().collect();
     let options = Options::new(args);
-    trace!("Options: {options:?}");
+    trace!("Options: {options:#?}");
     let compiler = CompilerContext::from_options(options);
-    trace!("Compiler empty: {compiler:?}");
+    trace!("Compiler empty: {compiler:#?}");
     // compiler.plan_jobs();
-    trace!("Compiler setup: {compiler:?}");
+    trace!("Compiler setup: {compiler:#?}");
     // compiler.run_job_loop().await;
-    // trace!("Compiler finished: {compiler:?}");
+    // trace!("Compiler finished: {compiler:#?}");
     Ok(())
 }
