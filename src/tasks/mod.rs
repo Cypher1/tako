13db4b509e6e778b29a6f2d978e019525d6a89f7
J Pratt <jp10010101010000@gmail.com>
2022-11-06 18:31:54 +1100

    A stream implementation (almost working)

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -4,14 +4,16 @@ use crate::error::{Error, TError};
 use crate::tokens::Token;
 use async_trait::async_trait;
 use std::collections::HashMap;
+use tokio_stream::{StreamExt, StreamMap, Stream};
 use tokio::sync::mpsc;
-use tokio_stream::StreamMap;
+use std::pin::Pin;
+use std::sync::{Arc, Mutex};
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum TaskState<T, E: std::error::Error> {
     /// Place holder to record that a job is already running and shouldn't need to be run again
     /// unless invalidated.
-    Started, // Include a handle to the result?
+    New,
     Running(Vec<T>), // Include a handle to the result?
     Success(Vec<T>), // Include a handle to the result?
     // Uncachable result, e.g. side effecting like saving a file
@@ -41,7 +43,7 @@ pub struct TaskId {
 pub struct TaskManager<T: Task> {
     // TODO: Each task should get its own channel!!!
     // Use https://docs.rs/tokio-stream/latest/tokio_stream/struct.StreamMap.html
-    result_store: TaskResults<T>,
+    result_store: Arc<Mutex<TaskResults<T>>>,
     task_reporters: StreamMap<T, T::Output>,
     task_receiver: ReceiverFor<T>,
     result_sender: SenderFor<T>,
@@ -51,7 +53,7 @@ pub struct TaskManager<T: Task> {
 impl<T: Task> TaskManager<T> {
     pub fn new(task_receiver: ReceiverFor<T>, result_sender: SenderFor<T>) -> Self {
         Self {
-            result_store: TaskResults::new(),
+            result_store: Arc::new(Mutex::new(TaskResults::new())),
             task_reporters: StreamMap::new(),
             task_receiver,
             result_sender,
@@ -59,24 +61,22 @@ impl<T: Task> TaskManager<T> {
     }
 
     pub async fn run_loop(&mut self) {
-        let (result_or_error_sender, result_or_error_receiver) = mpsc::unbounded_channel::<(TaskId<T>, Result<T::Output, Error>)>();
         tokio::spawn(async move {
-            while let Some(result) = result_or_error_receiver.recv().await {
-                let tasks = tasks_mutex.lock().expect("Can get tasks");
-                let mut task_entry = &mut tasks.entry(task).or_insert(TaskState::Started);
-                *task_entry = match result {
+            while let Some((task, result)) = self.task_reporters.next().await {
+                let result_store = self.result_store.lock().expect("Should be able to get result store");
+                let mut current_results = &mut result_store.entry(task).or_insert(TaskState::New);
+                *current_results = match result {
                     // TODO: Accumulate results
                     Ok(result) => {
                         let res = if T::RESULT_IS_CACHABLE {
-                            let results_so_far = match task_entry.value() {
-                                TaskState::SuccessUncachable => Vec::new(),
-                                TaskState::Success(_) => Vec::new(),
-                                TaskState::Started => Vec::new(),
-                                TaskState::Running(partials) => partials,
-    // Uncachable result, e.g. side effecting like saving a file
+                            let results_so_far = match **current_results {
+                                TaskState::SuccessUncachable => Vec::new(), // How did this happen?
+                                TaskState::New | TaskState::Success(_) | TaskState::Failure(_) => Vec::new(), // A new task or a redo...
+                                TaskState::Running(partials) => partials, // Continuing...
                             };
                             TaskState::Running(result.clone())
                         } else {
+                            // Uncachable result, e.g. side effecting like saving a file
                             TaskState::SuccessUncachable
                         };
                         self.result_sender.send(result);
@@ -87,18 +87,43 @@ impl<T: Task> TaskManager<T> {
             }
         });
 
-        while let Some(task) = self.task_receiver.recv().await {
-            use std::collections::hash_map::Entry;
-            match task_entry {
-                // Occupied(Invalidated) | // Restart
-                Entry::Vacant(entry) => {
-                    // Tasks will report that they are running. Do not report them here.
-                    task.perform(result_or_error_sender.clone()).await;
+        while let Some(task) = self.task_receiver.recv().await { // Get a new job from 'upstream'.
+            let status = {
+                let result_store = self.result_store.lock().expect("Should be able to get result store");
+                result_store.get(&task).cloned()
+            };
+            let status = status.unwrap_or(TaskState::New);
+            match status {
+                TaskState::Success(results) => {
+                    for result in results {
+                        self.result_sender.send(result);
+                    }
+                    continue; // i.e. go look for another task.
                 }
-                Entry::Occupied(_) => {} // Already running
+                TaskState::Running(_partial_results) => {
+                    // TODO: Consider...
+                    // We 'know' these are good, but there's more work to do
+                    // for result in partial_results {
+                        // self.result_sender.send(result);
+                    // }
+                    // Continue on and re-launch the job, duplicated work should not propagate if
+                    // completed.
+                }
+                _ => {}
             }
+            // Launch the job!!!
+            let (result_or_error_sender, mut result_or_error_receiver) =
+                mpsc::unbounded_channel::<Result<T::Output, Error>>();
+            // Convert the channels to a `Stream`.
+            let result_or_error_receiver = Box::pin(async_stream::stream! {
+                while let Some(item) = result_or_error_receiver.recv().await {
+                    yield item;
+                }
+            }) as Pin<Box<dyn Stream<Item = usize> + Send>>;
+
+            // Tasks will report that they are running. Do not report them here.
+            task.perform(result_or_error_sender).await;
         }
-        todo!();
     }
 }
 
