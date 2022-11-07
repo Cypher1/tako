fe5603a1d94aa3ec32a4b5ae9c86c2623c8540ad
J Pratt <jp10010101010000@gmail.com>
2022-11-07 23:19:03 +1100

    Avoid some duplicated work

--- src/ast.rs
+++ src/ast.rs
@@ -6,6 +6,68 @@ use crate::location::Location;
 // TODO: Use macro for defining and registering each of these.
 // TODO: A lambda calculus impl.
 
+trait Contains<T> {
+    fn get_all(&self) -> &Vec<T>;
+    fn get_all_mut(&mut self) -> &mut Vec<T>;
+    fn add(&mut self, value: T) -> TypedIndex<T> {
+        TypedIndex::new(self.get_all_mut(), value).expect("Should never have that many AstNodes...")
+    }
+    fn get(&self, id: TypedIndex<T>) -> &T {
+        id.get(self.get_all())
+    }
+    fn get_mut(&mut self, id: TypedIndex<T>) -> &mut T {
+        id.get_mut(self.get_all_mut())
+    }
+}
+
+macro_rules! make_contains(
+    { $field:ident, $type:ty, $id_type: ident } => {
+        impl Contains<$type> for Ast {
+            fn get_all(&self) -> &Vec<$type> {
+                &self.$field
+            }
+            fn get_all_mut(&mut self) -> &mut Vec<$type> {
+                &mut self.$field
+            }
+        }
+        type $id_type = TypedIndex<$type>;
+     };
+);
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Node {
+    pub name: TypedIndex<Symbol>,
+    pub id: NodeData,
+    pub location: Location,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+pub enum NodeData {
+    // TODO: consider how to split this up.
+    // Use a Array of Enums Structs to Struct of Arrays (i.e. AoES2SoA).
+    Symbol(SymbolId),
+    Call(CallId),
+    Definition(DefinitionId),
+    Primitive(PrimitiveId),
+}
+
+#[derive(Clone, Default, Debug)]
+pub struct Ast {
+    // Abstract syntax tree... forest
+    pub roots: Vec<NodeId>,
+    pub nodes: Vec<Node>,
+    pub calls: Vec<Call>,
+    pub symbols: Vec<Symbol>,
+    pub definitions: Vec<Definition>,
+    pub primitives: Vec<Primitive>,
+}
+
+make_contains!(nodes, Node, NodeId);
+make_contains!(calls, Call, CallId);
+make_contains!(symbols, Symbol, SymbolId);
+make_contains!(definitions, Definition, DefinitionId);
+make_contains!(primitives, Primitive, PrimitiveId);
+
 type StrId = String; // TODO: replace with an interned string id.
 
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -13,7 +75,6 @@ pub struct Symbol {
     node: NodeId,
     name: TypedIndex<StrId>, // index into the file
 }
-pub type SymbolId = TypedIndex<Symbol>;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Call {
@@ -21,7 +82,6 @@ pub struct Call {
     pub inner: NodeId,
     pub args: Vec<NodeId>, // TODO: Short vec
 }
-pub type CallId = TypedIndex<Call>;
 
 impl Call {
     #[cfg(test)]
@@ -40,40 +100,9 @@ pub struct Definition {
     pub name: TypedIndex<StrId>,
     pub implementation: NodeId,
 }
-pub type DefinitionId = TypedIndex<Definition>;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Primitive {
     node: NodeId,
     pub value: (), // TODO: ???
-}
-pub type PrimitiveId = TypedIndex<Primitive>;
-
-#[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
-pub enum NodeData {
-    // TODO: consider how to split this up.
-    // Use a Array of Enums Structs to Struct of Arrays (i.e. AoES2SoA).
-    Symbol(SymbolId),
-    Call(CallId),
-    Definition(DefinitionId),
-    Primitive(PrimitiveId),
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Node {
-    pub name: TypedIndex<Symbol>,
-    pub id: NodeData,
-    pub location: Location,
-}
-pub type NodeId = TypedIndex<Node>;
-
-#[derive(Clone, Default, Debug)]
-pub struct Ast {
-    // Abstract syntax tree... forest
-    pub roots: Vec<NodeId>,
-    pub nodes: Vec<Node>,
-    pub calls: Vec<Call>,
-    pub symbols: Vec<Symbol>,
-    pub definitions: Vec<Definition>,
-    pub primitives: Vec<Primitive>,
 }
