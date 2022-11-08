36901b70e3ede3ab05041a17a1e153324a31c68c
J Pratt <jp10010101010000@gmail.com>
2022-11-08 22:21:06 +1100

    Update token tests

--- src/tokens.rs
+++ src/tokens.rs
@@ -207,8 +207,6 @@ pub fn lex_head(characters: &mut Characters) -> Token {
                 characters.next(); // Skip escaped quotes.
             }
         }
-        // Pass the quote
-        characters.next();
     };
     let length = characters
         .index()
@@ -257,32 +255,32 @@ mod tests {
     #[test]
     fn lex_head_number() {
         let contents = setup("123");
-        let chars = Characters::new(&contents);
-        let (tok, _) = lex_head(chars);
+        let mut chars = Characters::new(&contents);
+        let tok = lex_head(&mut chars);
         assert_eq!(tok.kind, NumLit);
     }
 
     #[test]
     fn lex_head_symbol() {
         let contents = setup("a123");
-        let chars = Characters::new(&contents);
-        let (tok, _) = lex_head(chars);
+        let mut chars = Characters::new(&contents);
+        let tok = lex_head(&mut chars);
         assert_eq!(tok.kind, Sym);
     }
 
     #[test]
     fn lex_head_operator() {
         let contents = setup("-a123");
-        let chars = Characters::new(&contents);
-        let (tok, _) = lex_head(chars);
+        let mut chars = Characters::new(&contents);
+        let tok = lex_head(&mut chars);
         assert_eq!(tok.kind, Op);
     }
 
     #[test]
     fn lex_head_num_and_newline_linux() {
         let contents = setup("\n12");
-        let chars = Characters::new(&contents);
-        let (tok, _) = lex_head(chars);
+        let mut chars = Characters::new(&contents);
+        let tok = lex_head(&mut chars);
         assert_eq!(tok.kind, NumLit);
         assert_eq!(tok.start, 1);
     }
@@ -290,8 +288,8 @@ mod tests {
     #[test]
     fn lex_head_num_and_newline_windows() {
         let contents = setup("\r\n12");
-        let chars = Characters::new(&contents);
-        let (tok, _) = lex_head(chars);
+        let mut chars = Characters::new(&contents);
+        let tok = lex_head(&mut chars);
         assert_eq!(tok.kind, NumLit);
         assert_eq!(tok.start, 2);
     }
@@ -300,8 +298,8 @@ mod tests {
     fn lex_head_num_and_newline_old_mac() {
         // For mac systems before OSX
         let contents = setup("\r12");
-        let chars = Characters::new(&contents);
-        let (tok, _) = lex_head(chars);
+        let mut chars = Characters::new(&contents);
+        let tok = lex_head(&mut chars);
         assert_eq!(tok.kind, NumLit);
         assert_eq!(tok.start, 1);
     }
@@ -310,23 +308,23 @@ mod tests {
     fn lex_head_escaped_characters_in_string() {
         // TODO: De escape them.
         let contents = setup("'\\n\\t2\\r\\\'\"'");
-        let chars = Characters::new(&contents);
-        let (tok, _) = lex_head(chars);
+        let mut chars = Characters::new(&contents);
+        let tok = lex_head(&mut chars);
         assert_eq!(tok.kind, StringLit);
-        assert_str_eq!(tok.get_str(&contents), "\n\t2\r\'\"");
+        assert_str_eq!(tok.get_str(&contents), "\'\\n\\t2\\r\\'\"\'");
     }
 
     #[test]
     fn lex_head_call() {
         let contents = setup("x()");
-        let chars = Characters::new(&contents);
-        let (tok, chars2) = lex_head(chars);
+        let mut chars = Characters::new(&contents);
+        let tok = lex_head(&mut chars);
         assert_eq!(tok.kind, Sym);
         assert_str_eq!(tok.get_str(&contents), "x");
-        let (tok, chars3) = lex_head(chars2);
+        let tok = lex_head(&mut chars);
         assert_eq!(tok.kind, OpenBracket);
         assert_str_eq!(tok.get_str(&contents), "(");
-        let (tok, _) = lex_head(chars3);
+        let tok = lex_head(&mut chars);
         assert_eq!(tok.kind, CloseBracket);
         assert_str_eq!(tok.get_str(&contents), ")");
     }
@@ -334,14 +332,15 @@ mod tests {
     #[test]
     fn lex_head_strings_with_operators() {
         let contents = setup("!\"hello world\"\n7");
-        let chars = Characters::new(&contents);
-        let (tok, chars2) = lex_head(chars);
+        let mut chars = Characters::new(&contents);
+        let tok = lex_head(&mut chars);
         assert_eq!(tok.kind, Op);
         assert_str_eq!(tok.get_str(&contents), "!");
-        let (tok, chars3) = lex_head(chars2);
+        let tok = lex_head(&mut chars);
         assert_eq!(tok.kind, StringLit);
-        assert_str_eq!(tok.get_str(&contents), "hello world");
-        let (tok, _) = lex_head(chars3);
+        // The token-izer is not responsible for un-escaping...
+        assert_str_eq!(tok.get_str(&contents), "\"hello world\"");
+        let tok = lex_head(&mut chars);
         assert_eq!(tok.kind, NumLit);
         assert_str_eq!(tok.get_str(&contents), "7");
     }
@@ -359,7 +358,7 @@ mod tests {
                 .collect::<Vec<TokenType>>(),
             expected
         );
-        let expected_strs = vec!["(", "hello world", ")"];
+        let expected_strs = vec!["(", "\"hello world\"", ")"];
         assert_eq!(
             tokens
                 .iter()
@@ -382,7 +381,7 @@ mod tests {
                 .collect::<Vec<TokenType>>(),
             expected
         );
-        let expected_strs = vec!["!", "hello world", "7"];
+        let expected_strs = vec!["!", "\"hello world\"", "7"];
         assert_eq!(
             tokens
                 .iter()
