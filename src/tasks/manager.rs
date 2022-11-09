e65e5039bf60f860572603b48b3fbca7cd1b15e9
J Pratt <jp10010101010000@gmail.com>
2022-11-09 21:57:46 +1100

    Select makes this so much clearer

--- src/tasks/manager.rs
+++ src/tasks/manager.rs
@@ -1,10 +1,9 @@
 use crate::error::Error;
 
-use log::trace;
+use log::{debug, trace};
 use std::collections::HashMap;
 use std::fmt::Debug;
-use std::sync::{Arc, Mutex};
-use tokio::sync::{mpsc, watch, RwLock};
+use tokio::sync::{mpsc, watch};
 
 use super::status::*;
 use super::task_trait::*;
@@ -34,10 +33,18 @@ pub struct TaskManagerRegistration {
 }
 
 #[derive(Debug, Clone)]
-pub enum StatusReport {
-    StartingUp(TaskKind),
-    StatsUpdate { kind: TaskKind, stats: TaskStats },
-    ShuttingDown(TaskKind),
+pub struct StatusReport {
+    kind: TaskKind,
+    stats: TaskStats,
+}
+
+impl StatusReport {
+    pub fn new(kind: TaskKind) -> Self {
+        Self {
+            kind,
+            stats: TaskStats::default(),
+        }
+    }
 }
 
 #[derive(Debug, Default)]
@@ -48,13 +55,12 @@ pub struct ManagerConfig {
 
 #[derive(Debug)]
 pub struct TaskManager<T: Task> {
-    result_store: Arc<Mutex<TaskResults<T>>>,
     task_receiver: TaskReceiverFor<T>,
-    status_report_sender: watch::Sender<StatusReport>,
     result_sender: TaskSenderFor<T>,
-    // status_sender: mpsc::Sender<ManagerStats>,
-    stats: Arc<Mutex<TaskStats>>,
-    config: Arc<RwLock<ManagerConfig>>, // Use a RwLock so that reads don't block.
+    status_report_sender: watch::Sender<StatusReport>,
+    result_store: TaskResults<T>,
+    stats: TaskStats,
+    config: ManagerConfig,
 }
 
 impl<T: Debug + Task + 'static> TaskManager<T> {
@@ -72,171 +78,158 @@ impl<T: Debug + Task + 'static> TaskManager<T> {
         config: ManagerConfig,
     ) -> Self {
         Self {
-            result_store: Arc::new(Mutex::new(TaskResults::new())),
             task_receiver,
             result_sender,
             status_report_sender,
-            stats: Arc::new(Mutex::new(TaskStats::default())),
-            config: Arc::new(RwLock::new(config)),
+            result_store: TaskResults::new(),
+            stats: TaskStats::default(),
+            config,
         }
+    }
+
+    pub async fn handle_update(&mut self, task: T, update: Update<T::Output, Error>) {
+        trace!(
+            "{} received update from task: {task:#?} {update:#?}",
+            Self::task_name()
+        );
+        let caching_enabled = !self.config.disable_caching;
+        let mut current_results = self
+            .result_store
+            .entry(task)
+            .or_insert_with(TaskStatus::new);
+        let mut is_complete = false;
+        let mut error = None;
+        let results_so_far = &mut current_results.results;
+        match update {
+            Update::NextResult(res) => {
+                self.stats.total_num_results += 1;
+                self.result_sender
+                    .send(res.clone())
+                    .expect("Should be able to send results");
+                if caching_enabled {
+                    results_so_far.push(res);
+                }
+            }
+            Update::FinalResult(res) => {
+                self.stats.total_num_results += 1;
+                self.stats.num_succeeded += 1;
+                is_complete = true;
+                self.result_sender
+                    .send(res.clone())
+                    .expect("Should be able to send results");
+                if caching_enabled {
+                    results_so_far.push(res);
+                }
+            }
+            Update::Complete => {
+                self.stats.num_succeeded += 1;
+                is_complete = true;
+            }
+            Update::Failed(err) => {
+                self.stats.num_failed += 1;
+                is_complete = true; // For completeness...?
+                error = Some(err);
+            }
+        };
+        current_results.state = match (error, is_complete) {
+            (Some(err), _is_complete) => TaskState::Failure(err),
+            (None, /*is_complete*/ true) => TaskState::Complete,
+            (None, /*is_complete*/ false) => TaskState::Partial,
+        };
+    }
+
+    pub async fn handle_new_task(
+        &mut self,
+        result_or_error_sender: &mpsc::UnboundedSender<(T, Update<T::Output, Error>)>,
+        task: T,
+    ) {
+        // Get a new job from 'upstream'.
+        // trace!("{} received task: {task:#?}", Self::task_name());
+        {
+            self.stats.num_requests += 1;
+        }
+        let status = {
+            // We'll need to forward these on, so we can clone now and drop the result_store lock earlier!
+            let status = self
+                .result_store
+                .entry(task.clone())
+                .or_insert_with(TaskStatus::new);
+            if status.state != TaskState::New {
+                self.stats.num_already_running += 1;
+                return; // Done: Already running.
+            }
+            status.state = TaskState::Running;
+            status.clone()
+        };
+        match (&status.state, T::RESULT_IS_CACHABLE) {
+            // TODO: Consider that partial results 'should' still be safe to re-use and could pre-start later work.
+            /* TaskState::Partial | */
+            (TaskState::Complete, true) => {
+                self.stats.num_cached += 1;
+                trace!("{} cached task: {task:#?}", Self::task_name());
+                for result in status.results {
+                    self.result_sender
+                        .send(result)
+                        .expect("Should be able to send results");
+                }
+                return; // Done: Finished.
+            }
+            (TaskState::Complete, false) => {
+                trace!(
+                    "{} un-cacheable (will re-run): {task:#?}",
+                    Self::task_name()
+                );
+            }
+            // Continue on and re-launch the job, duplicated work should not propagate if completed.
+            _ => {
+                trace!(
+                    "{} task with status {status:#?}: {task:#?}",
+                    Self::task_name()
+                );
+            }
+        }
+        // Launch the job!!!
+        let result_or_error_sender = result_or_error_sender.clone();
+        tokio::spawn(async move {
+            // Tasks will report that they are running. Do not report them here.
+            task.perform(result_or_error_sender).await;
+        });
     }
 
     pub async fn run_loop(&mut self) {
         trace!("{} starting run_loop", Self::task_name());
         let (result_or_error_sender, mut result_or_error_receiver) =
             mpsc::unbounded_channel::<(T, Update<T::Output, Error>)>();
-        let stats = self.stats.clone();
-        let result_store = self.result_store.clone();
-        let result_sender = self.result_sender.clone();
-        let config = self.config.clone();
-        tokio::spawn(async move {
-            trace!("{}: Waiting for results...", Self::task_name());
-            while let Some((task, update)) = result_or_error_receiver.recv().await {
-                trace!(
-                    "{} received update from task: {task:#?} {update:#?}",
-                    Self::task_name()
-                );
-                // Reading from an RwLock should be near instant unless there is writing occuring.
-                let caching_enabled = !config.read().await.disable_caching;
-                let mut result_store = result_store
-                    .lock()
-                    .expect("Should be able to get the result store");
-                let mut current_results = result_store.entry(task).or_insert_with(TaskStatus::new);
-                let mut is_complete = false;
-                let mut error = None;
-                {
-                    let results_so_far = &mut current_results.results;
-                    let mut stats = stats
-                        .lock()
-                        .expect("Should be able to get task stats store");
-                    match update {
-                        Update::NextResult(res) => {
-                            stats.total_num_results += 1;
-                            result_sender
-                                .send(res.clone())
-                                .expect("Should be able to send results");
-                            if caching_enabled {
-                                results_so_far.push(res);
-                            }
-                        }
-                        Update::FinalResult(res) => {
-                            stats.total_num_results += 1;
-                            stats.num_succeeded += 1;
-                            is_complete = true;
-                            result_sender
-                                .send(res.clone())
-                                .expect("Should be able to send results");
-                            if caching_enabled {
-                                results_so_far.push(res);
-                            }
-                        }
-                        Update::Complete => {
-                            stats.num_succeeded += 1;
-                            is_complete = true;
-                        }
-                        Update::Failed(err) => {
-                            stats.num_failed += 1;
-                            is_complete = true; // For completeness...?
-                            error = Some(err);
-                        }
-                    };
-                }
-                current_results.state = match (error, is_complete) {
-                    (Some(err), _is_complete) => TaskState::Failure(err),
-                    (None, /*is_complete*/ true) => TaskState::Complete,
-                    (None, /*is_complete*/ false) => TaskState::Partial,
-                };
-            }
-            trace!(
-                "{} no more results... Finishing listening loop.",
-                Self::task_name()
-            );
-        });
 
-        trace!("{}: Waiting for tasks...", Self::task_name());
-        let stats = self.stats.clone();
-        'listening: while let Some(task) = self.task_receiver.recv().await {
-            // Get a new job from 'upstream'.
-            // trace!("{} received task: {task:#?}", Self::task_name());
-            {
-                let mut stats = stats
-                    .lock()
-                    .expect("Should be able to get task stats store");
-                stats.num_requests += 1;
+        loop {
+            trace!("{}: Waiting for tasks...", Self::task_name());
+            tokio::select! {
+                Some((task, update)) = result_or_error_receiver.recv() => {
+                    self.handle_update(task, update).await;
+                },
+                Some(task) = self.task_receiver.recv() => {
+                    self.handle_new_task(&result_or_error_sender, task).await;
+                },
+                else => break,
             }
-            let status = {
-                let mut result_store = self
-                    .result_store
-                    .lock()
-                    .expect("Should be able to get the result store");
-                // We'll need to forward these on, so we can clone now and drop the result_store lock earlier!
-                let status = result_store
-                    .entry(task.clone())
-                    .or_insert_with(TaskStatus::new);
-                if status.state != TaskState::New {
-                    let mut stats = stats
-                        .lock()
-                        .expect("Should be able to get task stats store");
-                    stats.num_already_running += 1;
-                    continue 'listening; // Already running.
-                }
-                status.state = TaskState::Running;
-                status.clone()
-            };
-            match (&status.state, T::RESULT_IS_CACHABLE) {
-                // TODO: Consider that partial results 'should' still be safe to re-use and could pre-start later work.
-                /* TaskState::Partial | */
-                (TaskState::Complete, true) => {
-                    let mut stats = stats
-                        .lock()
-                        .expect("Should be able to get task stats store");
-                    stats.num_cached += 1;
-                    trace!("{} cached task: {task:#?}", Self::task_name());
-                    for result in status.results {
-                        self.result_sender
-                            .send(result)
-                            .expect("Should be able to send results");
-                    }
-                    continue 'listening; // i.e. go look for another task.
-                }
-                (TaskState::Complete, false) => {
-                    trace!(
-                        "{} un-cacheable (will re-run): {task:#?}",
-                        Self::task_name()
-                    );
-                }
-                // Continue on and re-launch the job, duplicated work should not propagate if completed.
-                _ => {
-                    trace!(
-                        "{} task with status {status:#?}: {task:#?}",
-                        Self::task_name()
-                    );
-                }
-            }
-            // Launch the job!!!
-            let result_or_error_sender = result_or_error_sender.clone();
-            tokio::spawn(async move {
-                // Tasks will report that they are running. Do not report them here.
-                task.perform(result_or_error_sender).await;
-            });
         }
+        trace!(
+            "{} no more tasks or results. Terminating...",
+            Self::task_name()
+        );
+
         self.status_report_sender
-            .send(StatusReport::ShuttingDown(<T as Task>::TASK_KIND))
-            .expect("status_report_sender should shut down last");
-        let stats = self
-            .stats
-            .lock()
-            .expect("Should be able to get task stats store");
-        self.status_report_sender
-            .send(StatusReport::StatsUpdate {
+            .send(StatusReport {
                 kind: <T as Task>::TASK_KIND,
-                stats: stats.clone(),
+                stats: self.stats.clone(),
             })
-            .expect("status_report_sender should shut down last");
+            .unwrap_or_else(|err| {
+                debug!("Could not report final task manager stats: {}", err);
+            });
         trace!(
             "{} no more tasks... Finishing run_loop: {}",
             Self::task_name(),
-            format!("{:?}", &stats)
+            format!("{:?}", &self.stats)
         );
     }
 }
