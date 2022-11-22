055e7a5941e679e29cc1b3752727806b716bece5
J Pratt <jp10010101010000@gmail.com>
2022-11-23 07:28:00 +1100

    tmp

--- src/compiler_context.rs
+++ src/compiler_context.rs
@@ -16,13 +16,27 @@ impl Compiler {
         }
     }
 
-    pub async fn load_file() {
+    pub async fn watch_file(file: Pathbuf) {
         tokio::spawn(async move {
-            
         });
     }
 
-    pub async fn lex_file() {
+    pub async fn load_file(path: Pathbuf, results) {
+        let contents = std::fs::read_to_string(&path);
+        results.send(Ok((path.clone(), contents)));
+        tokio::spawn(async move {
+            Compiler::watch_file(path).await;
+        });
+    }
+
+    pub async fn lex_file(path: PathBuf, contents: Option<String>, results) {
+        tokio::spawn(async move {
+            let contents = contents.unwrap_or_else(||{
+                Compiler::load_file(path.clone()).await
+            );
+            let tokens = crate::tokens::lex_file(&contents);
+            results.send(Ok((path, tokens)));
+        });
     }
 
     pub async fn parse_file() {
