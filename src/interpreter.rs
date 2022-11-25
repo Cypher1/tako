c0dcf07de338f74c3f43f141ccf39979af060d9f
J Pratt <jp10010101010000@gmail.com>
2022-11-25 17:37:30 +1100

    Eval ops

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,5 +1,6 @@
 use crate::ast::*;
 use crate::error::TError;
+use crate::tokens::Symbol;
 use crate::literal_values::LiteralValues;
 use crate::primitives::Prim;
 use log::*;
@@ -30,6 +31,14 @@ pub fn run(path: &Path, ast: &Ast, root: Option<NodeId>) -> Result<Prim, TError>
 }
 
 impl<'a> Ctx<'a> {
+    pub fn eval2(&mut self, [l, r]: [Option<NodeId>; 2]) -> Result<[Prim; 2], TError> {
+        let l = l.expect("");
+        let r = r.expect("");
+        let l = self.eval(l)?;
+        let r = self.eval(r)?;
+        Ok([l, r])
+    }
+
     pub fn eval(&mut self, node: NodeId) -> Result<Prim, TError> {
         // TODO: ???
         let node = node.get(&self.ast.nodes);
@@ -37,20 +46,43 @@ impl<'a> Ctx<'a> {
             NodeData::NodeRef(_id) => todo!(),
             NodeData::NamedSymbol(_id) => todo!(),
             NodeData::Call(_id) => todo!(),
-            NodeData::Op(_id) => todo!(),
+            NodeData::Op(id) => self.eval_op(&node, id),
             NodeData::Definition(_id) => todo!(),
-            NodeData::Literal(id) => {
-                let (_id, lit) = id.get(&self.ast.literals);
-                let s = self.literals.get_str_by_loc(node.location.start);
-                Ok(match lit {
-                    Literal::Bool => todo!("{lit:?} {s:?}"),
-                    Literal::Numeric => {
-                        Prim::I32(s.expect("Should have string for literal").parse::<i32>()?)
-                    }
-                    Literal::Text => todo!("{lit:?} {s:?}"),
-                })
-            }
+            NodeData::Literal(id) => self.eval_lit(&node, id),
         }
+    }
+    pub fn eval_lit(&mut self, node: &Node, id: LiteralId) -> Result<Prim, TError> {
+        let (_node_id, lit) = id.get(&self.ast.literals);
+        let s = self.literals.get_str_by_loc(node.location.start);
+        Ok(match lit {
+            Literal::Bool => todo!("{lit:?} {s:?}"),
+            Literal::Numeric => {
+                Prim::I32(s.expect("Should have string for literal").parse::<i32>()?)
+            }
+            Literal::Text => todo!("{lit:?} {s:?}"),
+        })
+    }
+    pub fn eval_op(&mut self, _node: &Node, id: OpId) -> Result<Prim, TError> {
+        let (_node_id, op) = id.get(&self.ast.ops);
+        Ok(match op.op {
+            Symbol::Add => match self.eval2(op.args)? {
+                [Prim::I32(l), Prim::I32(r)] => Prim::I32(l+r),
+                _ => todo!(),
+            }
+            Symbol::Sub => match self.eval2(op.args)? {
+                [Prim::I32(l), Prim::I32(r)] => Prim::I32(l-r),
+                _ => todo!(),
+            }
+            Symbol::Mul => match self.eval2(op.args)? {
+                [Prim::I32(l), Prim::I32(r)] => Prim::I32(l*r),
+                _ => todo!(),
+            }
+            Symbol::Div => match self.eval2(op.args)? {
+                [Prim::I32(l), Prim::I32(r)] => Prim::I32(l/r),
+                _ => todo!(),
+            }
+            _ => todo!(),
+        })
     }
 }
 
