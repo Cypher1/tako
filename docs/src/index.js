bf27c68ce4ff0c978a4ca5ec8992c5578cb8c3ff
J Pratt <jp10010101010000@gmail.com>
2023-01-06 23:08:35 +1100

    Setup docs/demo with src and webpack

--- docs/src/index.js
+++ docs/src/index.js
@@ -2,7 +2,6 @@ import { Terminal } from 'xterm';
 import { WebLinksAddon } from 'xterm-addon-web-links';
 
 const terminal = new Terminal();
-const fitAddon = new FitAddon();
 terminal.loadAddon(new WebLinksAddon());
 terminal.open(document.getElementById('terminal'));
 terminal.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
