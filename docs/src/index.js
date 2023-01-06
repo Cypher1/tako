6ac5a5e4c5a1aafc09a47143953065157ee9c3f8
J Pratt <jp10010101010000@gmail.com>
2023-01-07 00:08:36 +1100

    Remove bad demo

--- docs/src/index.js
+++
@@ -1,7 +0,0 @@
-import { Terminal } from 'xterm';
-import { WebLinksAddon } from 'xterm-addon-web-links';
-
-const terminal = new Terminal();
-terminal.loadAddon(new WebLinksAddon());
-terminal.open(document.getElementById('terminal'));
-terminal.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
