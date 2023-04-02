57983b8655e5ec39843d6aa109ec5c1894896c9d
J Pratt <jp10010101010000@gmail.com>
2023-04-02 19:29:10 +1000

    Clean up

--- llamada-calculus/src/types.rs
+++ llamada-calculus/src/types.rs
@@ -12,8 +12,6 @@ pub enum Never {}
 
 impl std::fmt::Display for Never {
     fn fmt(&self, _f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            _ => unreachable!("Never can never be constructed"),
-        }
+        unreachable!("Never can never be constructed")
     }
 }
