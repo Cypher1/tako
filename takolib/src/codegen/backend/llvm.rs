b7a7a9fc1c87cf09424e03e9ac54c134900bb179
Jay Pratt <jp10010101010000@gmail.com>
2024-04-15 21:01:11 +1000

    Update dependencies

--- takolib/src/codegen/backend/llvm.rs
+++ takolib/src/codegen/backend/llvm.rs
@@ -168,7 +168,7 @@ impl<'ctx> BackendStateTrait for LlvmState<'ctx> {
 
     fn array_of_strings_type(&mut self) -> Self::PointerType {
         let char_star_type = self.char_ptr_type();
-        char_star_type.ptr_type(AddressSpace::default())
+        Context::ptr_type
     }
     fn const_int<T: Into<u64>>(&mut self, ty: Self::IntType, value: T) -> Self::Value {
         // TODO: Check bounds.
@@ -200,7 +200,7 @@ impl<'ctx> BackendStateTrait for LlvmState<'ctx> {
     }
 
     fn char_ptr_type(&mut self) -> Self::PointerType {
-        self.context.i8_type().ptr_type(AddressSpace::default())
+        Context::ptr_type
     }
 
     fn global_string(&mut self, value: &str) -> Self::PointerValue {
