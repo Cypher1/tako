94ee654d8d44543f5d52cb696d6068c7c7737ca2
Jay Pratt <jp10010101010000@gmail.com>
2024-09-29 12:01:12 +1000

    TMP

--- takolib/build.rs
+++ takolib/build.rs
@@ -1,18 +1,6 @@
-use cfgrammar::yacc::YaccKind;
-use lrlex::CTLexerBuilder;
+use std::path::PathBuf;
 
 fn main() {
-    CTLexerBuilder::new()
-        .lrpar_config(|ctp| {
-            ctp.yacckind(YaccKind::Grmtools)
-                .grammar_in_src_dir("tako.y")
-                .expect("tako.y failed to build")
-                .show_warnings(true)
-                .warnings_are_errors(true)
-                .error_on_conflicts(true)
-        })
-        .lexer_in_src_dir("tako.l")
-        .expect("tako.l failed to build")
-        .build()
-        .expect("tako's parser failed to build");
+    println!("cargo:rerun-if-changed=src");
+    rust_sitter_tool::build_parsers(&PathBuf::from("src/parser/mod.rs"));
 }
