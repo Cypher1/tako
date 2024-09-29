503224dbabaf5eeadcdb09c77e719b855f4c54e9
Jay Pratt <jp10010101010000@gmail.com>
2024-09-29 19:17:58 +1000

    Add a rough start at highlights

---
+++ tree-sitter-tako/queries/highlights.scm
@@ -0,0 +1,12 @@
+"func" @keyword
+"return" @keyword
+"break" @keyword
+"continue" @keyword
+"forall" @keyword
+"exists" @keyword
+"given" @keyword
+(type_identifier) @type
+(int_literal) @number
+(float_literal) @number
+(hex_literal) @number
+(function_declaration name: (identifier) @function)
